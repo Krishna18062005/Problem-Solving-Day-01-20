5886b0e054691f380284f1f9d90e26cbf92e4513
Krishna K <136406756+krishkrasher@users.noreply.github.com>
2024-04-23 20:27:36 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -19,33 +19,3 @@ public class Hello {
 
 	}
 }
-import java.util.*;
-public class Hello {
-
-    public static void main(String[] args) {
-		//day 1 prb 2
-		Scanner sc=new Scanner(System.in);
-		int a=sc.nextInt();
-		int b=sc.nextInt();
-		int c1=0;
-		String c=BinaryString(a);
-		String d=BinaryString(b);
-        int n=Math.min(c.length(),d.length());
-        if(d.length()>8||c.length()>8){
-            System.out.print(-1);
-        }else{
-		for(int i=0;i<n;i++){
-		    c1++;
-		    if(c.charAt(i)!=d.charAt(i))   break;
-		}//System.out.println(c+" "+d);
-		System.out.print((c1==1)?c1-1:c1);}
-	}
-	public static String BinaryString(int a){
-	    String k="";
-	    while(a>0){
-	        k+=(char)((a%2)+48);
-	        a/=2;
-	    }
-	    return k;
-	}
-}
