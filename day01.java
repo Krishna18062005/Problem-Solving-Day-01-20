3b0c2ca86404b09fc87a1531bcf133e97f6664f5
Krishna K <krish1826s@gmail.com>
2024-06-12 05:08:23 +0530

    Update and rename day1.java to day01.java

--- day01.java
+++ day01.java
@@ -20,3 +20,24 @@ class day1 {
     }
 
 }
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Day 1 
+		Scanner sc=new Scanner(System.in);
+		String b=sc.next();
+		String c=sc.next();
+		for(int i=0;i<c.length();i++){
+		    b=b.replace(c.substring(i,i+1),"*");}
+		
+		for(int i=0;i<b.length();i++){
+		    for(int j=i;j<b.length()+i;j++){
+		        
+		        System.out.print((j<b.length())?b.charAt(j):b.charAt(j-b.length()));
+		    }
+		    System.out.println();
+		}
+
+	}
+}
